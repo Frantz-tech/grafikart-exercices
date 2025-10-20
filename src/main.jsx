14cc3706dca697832f3e321df9a385af024e560b
Orsinet <frantz.o@icloud.com>
2025-10-20 11:10:17 +0200

    feat(framer motion): follow grafikart video, to use framer motion

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './exo 10 /App';
+import { App } from './exo 11 Framer Motion/App';
 import './index.css';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
